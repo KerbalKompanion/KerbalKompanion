402ca5dba353d66e3424c2019d38eaf953229d83
Noah Kamara <developer@noahkamara.com>
2020-08-23 19:24:20 +0200

    Added UI Tests

---
+++ KerbalKompanionTests/KerbalKompanionTests.swift
@@ -0,0 +1,33 @@
+//
+//  KerbalKompanionTests.swift
+//  KerbalKompanionTests
+//
+//  Created by Noah Kamara on 23.08.20.
+//  Copyright © 2020 Noah Kamara. All rights reserved.
+//
+
+import XCTest
+
+class KerbalKompanionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- Podfile
+++ Podfile
@@ -5,7 +5,6 @@ target 'KerbalKompanion' do
   # Comment the next line if you don't want to use dynamic frameworks
   # use_frameworks!
   pod 'PrimaryFlightDisplay'
-  pod 'AppCenter'
 
   # Pods for KerbalKompanion
 
